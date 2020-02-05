e425c8da3dab356adaca2cbac674e3aaa6cf9f51
RaphaelKoschig <52168525+RaphaelKoschig@users.noreply.github.com>
2020-02-05 07:24:08 +0100

    MAJ 04/02/2020 15h50

--- views/components/header.php
+++ views/components/header.php
@@ -1,26 +1,27 @@
 <div id="mainmenu">
-  <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+  <nav class="navbar navbar-expand-lg navbar-light bg-primary justify-content-between">
     <a class="navbar-brand" href="#">PigTycoon</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li>
-          <a class="nav-link" href="index.php">Accueil</a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php"><img src="./public/images/icons/home.svg" alt="icône accueil" class="navbar-icons">Accueil</a>
         </li>
         <li>
-          <a class="nav-link" href="index.php?action=listPigs">Cochons</a>
+          <a class="nav-link" href="index.php?action=listPigs"><img src="./public/images/icons/pig.svg" alt="icône cochon" class="navbar-icons">Cochons</a>
         </li>
         <li>
-          <a class="nav-link" href="index.php?action=society">Société</a>
+          <a class="nav-link" href="index.php?action=society"><img src="./public/images/icons/briefcase.svg" alt="icône société" class="navbar-icons">Société</a>
         </li>
         <li>
-          <a class="nav-link" href="index.php?action=contact">Contact</a>
+          <a class="nav-link" href="index.php?action=contact"><img src="./public/images/icons/envelope.svg" alt="icône contact" class="navbar-icons">Contact</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <img src="./public/images/icons/gear.svg" alt="icône gestion" class="navbar-icons">
             Gestion
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -38,8 +39,8 @@
 
 <script type="text/javascript">
   $(document).ready(function() {
-    // get current URL path and assign 'active' class
-    var pathname = window.location.pathname;
-    $('.navbar-nav > li > a[href="' + pathname + '"]').parent().addClass('active');
+    var pathname = window.location;
+    console.log(pathname)
+    $('a[href="'+pathname+'"]').parent().addClass('active');
   })
 </script>
