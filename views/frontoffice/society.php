ac7a228549de46566ced03bac177b319b443dec9
RaphaelKoschig <52168525+RaphaelKoschig@users.noreply.github.com>
2020-02-08 18:58:48 +0100

    MAJ 07/08/2020 15h30

--- views/frontoffice/society.php
+++ views/frontoffice/society.php
@@ -1,11 +1,23 @@
 <?php $title = 'Notre entreprise'; ?>
 
 <?php ob_start(); ?>
-<h1>PigTycoon</h1>
-<p>Notre histoire</p>
 
-<div>
-    <h3>Des trucs</h3>
+<div class="container" id="society-presentation">
+    <div class="row justify-content-center">
+        <h1>PigTycoon</h1>
+    </div>
+    <div class="row text-center">
+        <div class="col">
+            <img src="./public/images/others/oldpigfarm.png" alt="">
+        </div>
+        <div class="col center-block">
+            <p>Entreprise d'élevage de cochons depuis plus de 100 ans, nous disposons du savoir-faire et de la passion du métier pour vous procurer des porcs d'excellente qualité.</p>
+        </div>
+    </div>
+    <div class="row text-center">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur laudantium repudiandae incidunt commodi nam aspernatur est quis itaque, saepe, explicabo ipsam labore facilis vero. Tenetur sunt perferendis animi reprehenderit. Voluptatem?</p>
+    </div>
+
 </div>
 
 <?php $content = ob_get_clean(); ?>
